e318fb5007c7c4eca23db9c89d611d8eb236d5d9
Pallavi Dani Bhalerao <112056045+DevPallaviDani@users.noreply.github.com>
2026-03-18 16:29:35 +0530

    update StatsCard AirCondition Div

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -35,29 +35,30 @@ const Dashboard = () => {
       <div className="flex-3 p-6">
         <Header />
 
-        <div className="grid grid-cols-12 gap-6 mt-4 auto-rows-[140px]">
+        <div className="grid grid-cols-12 gap-6 mt-4 auto-rows-min">
           {/* Main Weather */}
-          <WeatherCard weather={weather} className="col-span-6" />
+          <WeatherCard weather={weather} className="col-span-6 h-full" />
 
           {/* Right side forecast */}
-          <WeeklyForeCast className="col-span-6" />
+          <WeeklyForeCast className="col-span-6 h-full" />
 
           {/* Chart */}
-          <ForecastCard foreCastData={foreCast} />
+          <ForecastCard className="col-span-12" foreCastData={foreCast} />
 
           {/* Stats */}
-          <div className="col-span-8 rounded-xl border-gray-100 hover:shadow-lg transition flex flex-col justify-between">
+          <div className="col-span-8 bg-white rounded-xl shadow border border-gray-100 p-4 flex flex-col gap-4">
             <div className="flex justify-between items-center px-4 pt-4">
               <h2 className="font-semibold mb-4">AIR CONDITION</h2>
-              <p 
-              className="text-lg mb-2 p-1 text-gray-50 rounded-xl bg-blue-500
-              hover:cursor-pointer">See More</p>
+              <p
+                className="text-lg mb-2 p-1 text-gray-50 rounded-xl bg-blue-500
+              hover:cursor-pointer"
+              >
+                See More
+              </p>
             </div>
 
             {weather && (
-              <div
-               className="grid grid-cols-2 gap-4 px-4 pb-4"
-               >
+              <div className="flex justify-between items-center ">
                 <StatsCard
                   title="Humanity"
                   value={weather.main.humidity}
@@ -71,12 +72,12 @@ const Dashboard = () => {
                 <StatsCard
                   title="Visibility"
                   value={weather.visibility}
-                  icon="☀️"
+                  icon="👁"
                 />
                 <StatsCard
                   title="Pressure"
                   value={weather.main.pressure}
-                  icon="☀️"
+                  icon="🌡"
                 />
 
                 {/* <StatsCard title="Humidity" value="72%" icon="💧" />
